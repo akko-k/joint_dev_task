d979a20d17d8d0dded699ed30379475a65ebdbe9
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-06-12 00:57:00 +0900

    質問11-13に回答

--- task.rb
+++ task.rb
@@ -96,14 +96,20 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports.flatten!
+  sports.uniq!
 
+  sports.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+    end
+ 
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
-
+  
   # 以下に回答を記載
-
+  puts data.dig(:user , :name)
 end
 
 def q13
@@ -111,7 +117,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  p user_data.merge!(update_data)
 end
 
 def q14
