00e99d1a9965511532c8f7304cc56645b498aa4c
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-09-17 12:47:05 +0900

    Q20の回答を修正

--- task.rb
+++ task.rb
@@ -226,22 +226,24 @@ class UserQ20
 end
 
 class Zoo
-  attr_reader :entry_fee
   def initialize(name:, entry_fee:)
     @entry_fee = entry_fee
   end
   def info_entry_fee(user) 
+    age_specific_entry_fee =
     case user.age
     when 0..5
-      puts "#{user.name}さんの入場料金は #{entry_fee[:infant]} 円です。"
+      @entry_fee[:infant]
     when 6..12
-      puts "#{user.name}さんの入場料金は #{entry_fee[:children]} 円です。"
+      @entry_fee[:children]
     when 13..64
-      puts "#{user.name}さんの入場料金は #{entry_fee[:adult]} 円です。"
+      @entry_fee[:adult]
     when 65..120
-      puts "#{user.name}さんの入場料金は #{entry_fee[:senior]} 円です。"
+      @entry_fee[:senior]
     end
+    puts "#{user.name}さんの入場料金は #{age_specific_entry_fee} 円です。"
   end
+  
 end
 
 
